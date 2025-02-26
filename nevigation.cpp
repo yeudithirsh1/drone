ab93c16ab53960f2aa327f84c86cb1add6cdfdb4
yeudithirsh1 <yeudit.hirsh32610@gmail.com>
2025-02-26 18:44:12 +0200

    26/02/2025

--- nevigation.cpp
+++ nevigation.cpp
@@ -97,11 +97,11 @@ vector<Point> generatePointsOnLine(Point A, Point B, double step, bool flag) {
 
 vector<vector<Point>> zigzag(vector<vector<Point>> graph)
 {
-    int j = NULL, l, t = 0, m = graph[graph.size() - 1].size() - 1, n = graph[graph.size() - 1].size() - 1, s = 2, length = min(graph[0].size(), graph[graph.size() - 1].size());
-    bool flag = false, d1 = true, d2 = true, b;
+    int i, k, j = NULL, l, t = 0, m = graph[graph.size() - 1].size() - 1, n = graph[graph.size() - 1].size() - 1, s = 2, length = min(graph[0].size(), graph[graph.size() - 1].size());
+    bool flag = false, d1 = true, d2 = true, b= false;
 
     // לולאה עיקרית שמבצעת את הסריקה
-    for (int i = 0, k = graph.size() - 1; i < graph.size() && k >= 0; i++, k--)
+    for (i = 0, k = graph.size() - 1; i < graph.size() && k >= 0; i++, k--)
     {
         // חיבור בין נקודות בצלע הנוכחית
         for (j = t; j < length && n >= 0; j++)
@@ -112,36 +112,53 @@ vector<vector<Point>> zigzag(vector<vector<Point>> graph)
                 if (j + 1 < graph[i].size())
                     graph[i][j].next = &graph[i][j + 1];
                 else
+                {
                     if (i + 1 < graph.size())
                     {
                         graph[i][j].next = &graph[i + 1][0];
                         b = true;
+                        s = 1;
                     }
+                }
                 d2 = false;
             }
             else
             {
-                if (n >= 0 && k < graph.size() && n < graph[k].size())
+                if (k < graph.size() ) 
+                {
+                    if (j == length)
+                        s = 0;
                     graph[i][j].next = &graph[k][n];
+                }
                 d2 = true;
-                n--;
+				if (n - 2 >= 0)
+                    n-=2;
+                else
+                {
+                    if (n = 0)
+                        n = graph[k - 1].size() - 2;
+                    else
+                        n = graph[k - 1].size() - 1;
+                }
             }
         }
         cout << "------------------\n";
         // חיבור בין הנקודות בצלע השנייה בכיוון ההפוך
-        for (l = m; l > 0 && s <= graph[i].size(); l--)
+        for (l = m; l >= 0 && s <= graph[i].size(); l--)
         {
             cout << i << "," << k << "," << l << "," << m << "," << s << "," << d1 << "\n";
             if (d1)
             {
-                if (l - 1 >= 0)
+                if (l - 1 > 0)
                     graph[k][l].next = &graph[k][l - 1];
                 else
+                {
                     if (k > 0)
                     {
                         graph[k][l].next = &graph[k - 1][graph[k - 1].size() - 1];
                         flag = true;
                     }
+                }
                 d1 = false;
             }
             else
@@ -149,37 +166,47 @@ vector<vector<Point>> zigzag(vector<vector<Point>> graph)
                 if (s < graph[i].size() && k < graph.size() && s < graph[i].size())
                     graph[k][l].next = &graph[i][s];
                 d1 = true;
-                s++;
+                if (s + 2 <= graph[i].size() - 1)
+                    s++;
+                else
+                {
+                    if (s == graph[i].size() - 1)
+                        s = 1;
+                    else
+                        s = 0;
+                }
             }
         }
         cout << "------------------\n";
 
         // עדכון מצב כדי להחליף צלע
-        if (j < graph[i].size() && l <= 0)
+        if (j < graph[i].size() && ++l == 0)
         {
-            cout << j << "\n";
-            i--;
+            cout << i << "," << k << "," << j << "," << t << "," << n << "," << d2 << "," << length << "\n";
+            cout << i << "," << k << "," << l << "," << m << "," << s << "," << d1 << "," << length << "\n";
+            cout << "------------------\n";
             t = j;
-            flag = false;
-            length = graph[i].size();
-            m = graph[k - 1].size() - 1;
-            if (flag)
-                n = graph[k - 1].size() - 2;
+            if ((graph[i].size() - j) <= graph[k - 1].size())
+                length = j + (graph[i].size() - j);
             else
-                n = graph[k - 1].size() - 1;
+                length = graph[k - 1].size();
+            i--;
+            m = graph[k - 1].size() - 1;
+            cout << i << "," << k << "," << j << "," << t << "," << n << "," << d2 << "," << length << "\n";
+            cout << i << "," << k << "," << l << "," << m << "," << s << "," << d1 << "," << length << "\n";
+            cout << "------------------\n";
         }
         else
         {
-            if (l < graph[k].size() && j == graph[i].size())
+            if (l > 0 && j == graph[i].size())
             {
                 k--;
                 m = l;
-                length = graph[k].size();
-                t = 0;
-                if (b)
-                    s = 1;
+                if ((graph[k].size() - l) <= graph[i + 1].size())
+                    length = graph[k].size() - l;
                 else
-                    s = 0;
+                    length = graph[i + 1].size();
+                t = 0;
             }
             else
             {
@@ -187,6 +214,7 @@ vector<vector<Point>> zigzag(vector<vector<Point>> graph)
                 {
                     t = 0;
                     m = graph[k - 1].size() - 1;
+
                     if (b)
                         s = 1;
                     else
